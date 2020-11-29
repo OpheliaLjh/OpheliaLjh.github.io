7ef4d3a124a9e41d42f6ad4883c33d6ebe276ff5
olipq <ophelia.li@proquest.com>
2020-11-29 15:25:30 +0800

    upload all files

---
+++ assets/js/main.js
@@ -0,0 +1,120 @@
+/*
+	Strata by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	var $window = $(window),
+		$body = $('body'),
+		$header = $('#header'),
+		$nav = $('#nav'),
+
+		$footer = $('#footer'),
+		$main = $('#main'),
+		settings = {
+
+			// Parallax background effect?
+				parallax: true,
+
+			// Parallax factor (lower = more intense, higher = less intense).
+				parallaxFactor: 20
+
+		};
+	// Nav.
+	var $nav_a = $nav.find('a');
+
+	$nav_a
+		.addClass('scrolly')
+		.on('click', function() {
+
+			var $this = $(this);
+
+			// External link? Bail.
+			if ($this.attr('href').charAt(0) != '#')
+				return;
+
+			// Deactivate all links.
+			$nav_a.removeClass('active');
+
+			// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+			$this
+				.addClass('active');
+
+		})
+		.each(function() {
+
+			var	$this = $(this),
+				id = $this.attr('href'),
+				$section = $(id);
+
+			// No section for this link? Bail.
+			if ($section.length < 1)
+				return;
+
+			// Scrollex.
+			$section.scrollex({
+				mode: 'middle',
+				top: '0vh',
+				bottom: '0vh',
+				initialize: function() {
+
+					// Deactivate section.
+					$section.addClass('inactive');
+
+				},
+				enter: function() {
+
+					// Activate section.
+					$section.removeClass('inactive');
+
+					// No locked links? Deactivate all links and activate this section's one.
+					if ($nav_a.filter('.active-locked').length == 0) {
+
+						$nav_a.removeClass('active');
+						$this.addClass('active');
+
+					}
+
+					// Otherwise, if this section's link is the one that's locked, unlock it.
+					else if ($this.hasClass('active-locked'))
+						$this.removeClass('active-locked');
+
+				}
+			});
+
+		});
+	// Scrolly.
+	$('.scrolly').scrolly({
+		speed: 1000,
+		offset: function() {
+
+
+
+		}
+	});
+
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Touch?
+		if (browser.mobile) {
+
+			// Turn on touch mode.
+				$body.addClass('is-touch');
+
+			// Height fix (mostly for iOS).
+				window.setTimeout(function() {
+					$window.scrollTop($window.scrollTop() + 1);
+				}, 0);
+
+		}
+
+
+})(jQuery);
